801dfabc2417565c191425cb7b364ff8c181a626
jcostadev23 <carlos_jesus_costa@hotmail.com>
2023-06-22 23:54:58 +0100

    Original text content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,10 @@ export default function Home() {
     <main className="px-6 mx-auto">
       <p className="mt-8 mb-16 text-2xl text-center dark:text-black">
       Olá, chamo-me Lília Dionísio! <br/>
-      Desde muito pequena, que sou apaixonada por maquiagem.<br/> 
-      Sempre admirei aqueles que gostavam de se maquiar e tenho uma grande admiração por todos que seguiram seus sonhos e se tornaram profissionais.<br/> 
-      Um dia eu também quero ser como eles.<br/>
-      No entanto, nunca segui meu sonho por medo de fracassar...
-      Durante muito tempo, a maquiagem foi meu refúgio, minha luz no fim do túnel.<br/>
-      Em dias tristes e sombrios, maquiar-me apenas para tirar fotos era o que me restava,
-       e acredite, isso me fazia e continua me fazendo muito bem!<br/>
-      Portanto, acredito que a maquiagem vai muito além da beleza, 
-      superficialidade ou estética.<br/> Ela é um manifesto de autoestima.<br/>
-      E quando nos sentimos bem conosco mesmos, conseguimos afastar os medos.<br/>
-      Sinto que uma das minhas missões é fazer isso, maquiar por amor, por prazer!
+      Desde muito pequena que sempre fui louca por Maquilhagem, sempre admirei quem goste de se maquilhar, admiro imenso todas as que seguiram seus sonhos e se tornaram profissionais, um dia queria eu ser como elas nunca segui meu sonho por medo de Falhar...<br/> 
+      Durante muito tempo a Maquilhagem foi meu Refúgio, minha luz no fim do Túnel. Em dias tristes e obscuros, me maquilhar apenas para tirar fotos era o que me restava, e acredite, me fazia e me faz muito bem!<br/>
+      Então acredito que a Maquilhagem vai muito além da beleza, superficialidade ou estética, é um manifesto de autoestima. E a autoestima nos afasta dos medos.<br/>
+      Sinto que uma das minhas missões, é fazer isso, fazer por Amor, por Prazer!
         </p>
     </main>
   )
