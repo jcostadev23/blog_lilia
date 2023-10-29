d7ca7674bc2def120fa8427b145a5426902120ca
jcostadev23 <carlos_jesus_costa@hotmail.com>
2023-10-29 06:33:13 +0000

    adding link to new page

--- src/app/components/mycard.tsx
+++ src/app/components/mycard.tsx
@@ -6,7 +6,8 @@ export default function MyCard() {
     <div className="card">
       <div className="w-full bg-cover bg-center m-8 flex flex-col items-center justify-center sm:flex-row">
         <section className="w-full sm:w-1/2 p-8">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima hic modi aperiam, natus obcaecati sunt odio quibusdam! Possimus eaque sint accusantium ipsum, reiciendis laborum? Fugiat soluta sit voluptatum ipsa eum rem voluptate, eos eligendi veritatis laudantium, est consectetur dignissimos, impedit recusandae ullam aut quae. Veritatis odit assumenda qui et voluptate!</p>
+          <p className="text-lg text-gray-800 font-semibold text-center leading-7 mt-4">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima hic modi aperiam, natus obcaecati sunt odio quibusdam! Possimus eaque sint accusantium ipsum, reiciendis laborum? Fugiat soluta sit voluptatum ipsa eum rem voluptate, eos eligendi veritatis laudantium, est consectetur dignissimos, impedit recusandae ullam aut quae. Veritatis odit assumenda qui et voluptate!</p>
         </section>
         <section className="w-full sm:w-1/2 p-8">
           <div className="flex flex-col items-center justify-center">
@@ -14,7 +15,10 @@ export default function MyCard() {
           </div>
         </section>
       </div>
-      <a className="font-bold ml-4" href={`/users/about/`}>Mais sobre meu trabalho {'\u2192'} &hearts; </a>
+      <div className="flex flex-col space-y-4">
+        <a className="font-bold ml-4" href={`/users`}>Mais sobre mim, {'\u2192'} &hearts; </a>
+        <a className="font-bold ml-4" href={`/users/works`}>Meus Trabalhos, {'\u2192'} &hearts; </a>
+      </div>
     </div>
   );
 }
