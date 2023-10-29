7a01a11a374304e02ecc7c8371b14113c7e69064
jcostadev23 <carlos_jesus_costa@hotmail.com>
2023-10-29 06:00:32 +0000

    navBar logo

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,16 +1,15 @@
+import Image from 'next/image'
 import { FaFacebook, FaInstagram, FaTiktok, FaWhatsapp } from 'react-icons/fa'
 
 export default function Navbar() {
     return (
-        <nav className="bg-pink-100 p-4 sticky top-0
+        <nav className="bg-pink-100 sticky top-0
         drop-shadow-xl z-10">
-            <div className="prose prose-xl mx-auto flex justify-between
-             flex-col sm:flex-row">
+            <div className="max-w-screen-xl p-4 mx-auto flex items-center justify-between sm:flex-row">
                 <a href='/' className='no-underline'>
-                    <h1 className='text-3xl text-white/100 font-bold place-content-start mb-2 md:mb-0'>Lilia Make Up </h1>
+                    <Image src="/images/logo.jpg" alt="logo" height={75} width={75} />
                 </a>
-                <div className='flex flex-row justify-center sm:justify-evenly align-middle
-                gap-4 text-white text-4xl lg:text-5xl'>
+                <div className='flex flex-row space-x-4 text-white text-4xl lg:text-5xl'>
                     <a className='text-white/90 hover:text-white' href="https://www.facebook.com/liliadionisio.93">
                         <FaFacebook />
                     </a>
