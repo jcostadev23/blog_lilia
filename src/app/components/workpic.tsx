ed1babbd855ea6c78aec656d09c94c03000510cd
jcostadev23 <carlos_jesus_costa@hotmail.com>
2023-10-29 23:04:13 +0000

    Changing Workpic method

--- src/app/components/workpic.tsx
+++ src/app/components/workpic.tsx
@@ -1,25 +1,24 @@
 "use client"
 import Image from "next/image";
+import allData from "../storage/allData"
+import Description from "./Description"
 
 export default function WorkPicture() {
-    const imagePaths = Array.from({ length: 14 }, (_, index) => (`/images/workpic/pic${index + 1}.jpg`));
 
     return (
         <div className='grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 m-10'>
-            {imagePaths.map((image, index) => (
-                <a className="flex flex-col items-center no-underline" key={index} href={`/users/${index + 1}`}>
+            {allData.map((data) => (
+                <a className="flex flex-col items-center no-underline" key={data.id} href={`/users/${data.id}`}>
                     <Image
                         className='transition-transform transform-origin-center transform hover:scale-125 duration-500 ease-in-out drop-shadow-xl
                          shadow-black rounded mt-4'
-                        src={image}
+                        src={data.image1}
                         width={200}
                         height={300}
                         alt={'work picture'}
                         priority={true}
                     />
-                    <p className="text-lg text-gray-800 font-semibold text-center leading-7 mt-4">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, ex! Atque aliquam maiores repellendus voluptatibus reprehenderit quibusdam obcaecati dolores quaerat.
-                    </p>
+                    <Description description={data.description} />
                 </a>
             ))}
         </div>
